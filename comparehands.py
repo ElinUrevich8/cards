9a6373f3251c565578b9c46e53bdda0e80edd6ba
Elin Urevich <elin.urevich@sisense.com>
2026-01-02 22:24:15 +0200

    create comparesment

--- comparehands.py
+++ comparehands.py
@@ -1,20 +1,26 @@
-from card import Rank, Hand, Card, Suit
-from strength import HandStrengthEvaluation, HandStrengthEvaluator, HandStrength
+from card import Hand
+from strength import EvaluatedHand
 
 class CompareHand:
-    def compare(self, hand1: Hand, hand2: Hand) -> int:
+    def beats(self, hand1: Hand, hand2: Hand) -> int:
         """Compare two hands and return:
         - 1 if hand1 is stronger
         - -1 if hand2 is stronger
         - 0 if they are equal
         """
-        strength1: HandStrength = HandStrengthEvaluator(hand1)
-        strength2: HandStrength = HandStrengthEvaluator(hand2)
+        evaluated_hand1 = EvaluatedHand(hand1)
+        evaluated_hand2 = EvaluatedHand(hand2)
 
-        if strength1.value > strength2.value:
+        if evaluated_hand1.strength() > evaluated_hand2.strength():
             return 1
-        elif strength1.value < strength2.value:
+        elif evaluated_hand1.strength() < evaluated_hand2.strength():
             return -1
+        # If strengths are equal, compare kickers
+        for kicker1, kicker2 in zip(evaluated_hand1.kickers(), evaluated_hand2.kickers()):
+            if kicker1 > kicker2:
+                return 1
+            elif kicker1 < kicker2:
+                return -1
 
         # Continue with other hand rankings...
 
